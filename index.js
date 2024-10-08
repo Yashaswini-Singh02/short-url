adc440e583db6b3f5e13b4f77687d0ddd691ab49
Yashaswini Singh Shaktawat <yashaswinisinghshaktawat@gmail.com>
2024-10-09 04:08:04 +0530

    feat: add short-url service

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url");
+const URL = require("./models/url");
+
+const app = express();
+const PORT = 8001;
+
+connectToMongoDB("mongodb://localhost:27017/short-url");
+
+app.use(express.json());
+// app.get("/:shortId", async (req, res) => {
+//   const shortId = req.params.shortId;
+//   const entry = await URL.findOneAndUpdate(
+//     {
+//       shortId,
+//     },
+//     {
+//       $push: {
+//         visitHistory: {
+//           timestamp: Date.now(),
+//         },
+//       },
+//     }
+//   );
+//   res.redirect(entry.redirectURL);
+// });
+app.use("/url", urlRoute);
+
+app.listen(PORT, () => console.log(`Serve Started at PORT ${PORT}`));
---
+++ routes/url.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const {
+  handleGenerateShortURL,
+  handleRedirectURL,
+  getHandleAnalytics,
+} = require("../controllers/url");
+const router = express.Router();
+
+router.post("/", handleGenerateShortURL);
+
+router.get("/:shortId", handleRedirectURL);
+
+router.get("/analytics/:shortId", getHandleAnalytics);
+
+module.exports = router;
